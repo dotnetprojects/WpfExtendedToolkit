f241331ba4e893f81f2844ec490f10a18eb8f12f
jogibear9988 <jochen.kuehner@gmx.de>
2016-11-13 14:36:25 +0100

    one more test to fix

--- Src/Xceed.Wpf.AvalonDock/AssemblyVersionInfo.cs
+++ Src/Xceed.Wpf.AvalonDock/AssemblyVersionInfo.cs
@@ -14,4 +14,7 @@
 
   ***********************************************************************************/
 
-[assembly: System.Reflection.AssemblyVersion("1.0.*")]
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
